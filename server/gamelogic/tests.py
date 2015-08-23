7a3a1d61b87f8b0bc172cfd57f5b7ce12b749c1a
Jonathan Yannes <jyannes@Jonathans-MacBook-Pro.local>
2015-08-23 16:38:51 -0400

    set up tests

--- server/gamelogic/tests.py
+++ server/gamelogic/tests.py
@@ -1,7 +1,4 @@
 import unittest
 
 class GameTest(unittest.TestCase):
-	def testtest(self):
-		self.assertTrue(True)
-
-unittest.main()
+	testnames = []
--- server/runtest.py
+++ server/runtest.py
@@ -1,9 +1,25 @@
+import unittest
+
+import gamelogic.tests
+
+def addTestsToSuite(suite, testcase):
+	for testname in testcase.testnames:
+		suite.addTest(testcase(testname))
+
+	return suite
+
 if __name__ == '__main__':
-	from os import system
+
 	from sys import argv
-	import unittest.main
+
+	suite = unittest.TestSuite()
 
 	if len(argv) > 1:
 		if argv[1] == 'gamelogic':
-			system('python gamelogic/tests.py')
+			suite = addTestsToSuite(suite, gamelogic.tests.GameTest)
+		else:
+			print 'Invalid argument'
+	else:
+		suite = addTestsToSuite(suite, gamelogic.tests.GameTest)
 
+	unittest.TextTestRunner().run(suite)
