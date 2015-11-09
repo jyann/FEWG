d0e7740bdf2ab84813c33186ff53a4f860bc14b0
Jonathan Yannes <jyannes@Jonathans-MacBook-Pro.local>
2015-11-09 15:02:57 -0500

    auto scroll log

--- client/webapp/js/log.js
+++ client/webapp/js/log.js
@@ -13,6 +13,9 @@
 				$rootScope.addToLog = function(type, msg){
 					$rootScope.$apply(function(){
 						ctrl.clientlog.push({'type':type,'msg':msg});
+						var newTop = $("div#logdiv").scrollTop() 
+									+ $(".log-item").height();
+						$("div#logdiv").scrollTop(newTop);
 					});
 				};
 			}],
